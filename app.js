742a7a78c37d3f6d21a48634fdb639181ef3ff8a
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-29 10:40:02 -0700

    added Movie table sync and first row

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const { Sequelize, Model } = require("sequelize");
 const sequelize = new Sequelize({ dialect: "sqlite", storage: "movies.db" });
 
 class Movie extends Model {}
-Movie.init();
+Movie.init({ title: Sequelize.STRING }, { sequelize });
 (async () => {
+  //creates or updates table based on Movie model
+  //await Movie.sync();
+  //will synchronize all tables based on models - follows CREATE TABLE IF NOT EXISTS statement rules
+  await sequelize.sync({ force: true }); //force: true  - follow DROP TABLE IF EXISTS which completely deletes the table
   try {
-    await sequelize.authenticate();
-    console.log("Connection to database successful.");
+    const movie = await Movie.create({ title: "Toy Story" });
+    console.log(movie.toJSON());
   } catch (err) {
     console.error("Error connecting to the database: ", err);
   }
