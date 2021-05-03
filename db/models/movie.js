4e6ad51a621c84927baf9c79de13ffdc29e48f26
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-02 18:01:57 -0700

    added data types to model properties

--- db/models/movie.js
+++ db/models/movie.js
@@ -2,7 +2,15 @@ const { Sequelize, Model } = require("sequelize");
 
 module.exports = (sequelize) => {
   class Movie extends Model {}
-  Movie.init({ title: Sequelize.STRING }, { sequelize });
+  Movie.init(
+    {
+      title: { type: Sequelize.STRING },
+      runtime: { type: Sequelize.INTEGER },
+      releaseDate: { type: Sequelize.DATEONLY },
+      isAvailableOnVHS: { type: Sequelize.BOOLEAN },
+    },
+    { sequelize }
+  );
 
   return Movie;
 };
