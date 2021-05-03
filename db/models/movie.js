4548f7e9a0a2db734d8ca5e754daa050cec34d63
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-02 18:19:30 -0700

    added default value to vhs

--- db/models/movie.js
+++ db/models/movie.js
@@ -4,10 +4,29 @@ module.exports = (sequelize) => {
   class Movie extends Model {}
   Movie.init(
     {
-      title: { type: Sequelize.STRING },
-      runtime: { type: Sequelize.INTEGER },
-      releaseDate: { type: Sequelize.DATEONLY },
-      isAvailableOnVHS: { type: Sequelize.BOOLEAN },
+      //following id is unnecessary, but this is good for understanding how to implement primaryKey values and autoIncrement
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false, // disallow null
+      },
+      runtime: {
+        type: Sequelize.INTEGER,
+        allowNull: false, // disallow null
+      },
+      releaseDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false, // disallow null
+      },
+      isAvailableOnVHS: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false, // disallow null
+        defaultValue: false, // set default value
+      },
     },
     { sequelize }
   );
